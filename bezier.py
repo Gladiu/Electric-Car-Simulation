4a232c77f22fe5deccbf20dadfb007091ba4ebbd
gladiu <maurycyk1@tlen.pl>
2021-11-11 13:20:18 +0000

    Image is begin plotted with matplotlib. Needs fixing as its in wrong orientation

--- bezier.py
+++ bezier.py
@@ -1,6 +1,24 @@
 import imageio
 import numpy as np
+import matplotlib.pyplot as plot
 
-im = imageio.imread('image/50opacity.png')
+def check_if_zero(array):
+    for i in range(len(array)):
+        if array[i] != 0:
+            return False
+    return True
+
+
+im = imageio.imread('image/test.png')
 im = np.array(im)
-print(im)
+
+x_array = []
+y_array = []
+
+for x in range(im.shape[0]):
+    for y in range(im.shape[1]):
+        if check_if_zero(im[x][y]) == False:
+            x_array.append(x)
+            y_array.append(y)
+plot.plot(x_array, y_array)
+plot.show()
